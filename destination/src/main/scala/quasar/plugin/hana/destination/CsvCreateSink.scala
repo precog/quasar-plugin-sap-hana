b173ee9e0e1ded03ba181fb9023e9c71b674ac05
Alissa Pajer <alissa.pajer@gmail.com>
2020-08-27 16:41:55 -0400

    wip

--- destination/src/main/scala/quasar/plugin/hana/destination/CsvCreateSink.scala
+++ destination/src/main/scala/quasar/plugin/hana/destination/CsvCreateSink.scala
@@ -91,7 +91,7 @@ private[destination] object CsvCreateSink {
         HC.createStatement(FS.execute(statement).map(_ => ()))
 
       chars.evalMap(v => {
-        logger.info(s">>>> v: $v")
+        logger.info(s">>>> v: ${v.toString.getBytes(java.nio.charset.Charset.forName("UTF-8")).toList}")
         val nonull = if (v == "") "null" else v
         insertStatement(nonull).flatMap(s => ConcurrentEffect[F].delay(logger.info(s"statement: $s")) >> connect(s).transact(xa))
       })
